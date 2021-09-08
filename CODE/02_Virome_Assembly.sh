24ae4cdd0102445d80c52129f07eb8b0eeb6ef45
Ema Graham <60945400+egrah3@users.noreply.github.com>
2021-09-08 15:11:12 -0500

    Update 02_Virome_Assembly.sh

--- CODE/02_Virome_Assembly.sh
+++ CODE/02_Virome_Assembly.sh
@@ -1,3 +1,7 @@
+### Title: 02_Virome_Assembly ###
+### Author: Ema H Graham ###
+### For Questions Email: ema.graham@huskers.unl.edu ###
+
 ############################################
 ## -------------------------------------- ##
 ## ------------- Description ------------ ##
